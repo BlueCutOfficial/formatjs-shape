3038fd9da70bae43fda6050b8e3492b2f7f6a43c
Marine Dunstetter <bluecut.official@gmial.com>
2025-01-24 10:29:14 +0100

    write unit tests

--- src/extension.ts
+++ src/extension.ts
@@ -10,36 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
 			const selection = editor.selection;
 
 			const selectedText = document.getText(selection); // include quotes
-			const trimedSelectedText = selectedText.slice(1, selectedText.length-1); // exclude quotes
-
-			let replacement = `{ defaultMessage: ${selectedText} }`;
-
-			// Handle template strings
-			const quasis = selectedText.match(/\$\{[^\}]*\}/mg);
-			if (quasis?.length) {
-				let i = 1;
-				const options = [];
-				for (const quasi of quasis) {
-					const expression = quasi.slice(2, quasi.length-1);
-
-					// Build an "ok" option name for the expression
-					let optionName = camelCase(expression.replace('this.', ''));
-					if (optionName.length > 20) {
-						optionName = `option${i}`;
-						i++;
-					}
-
-					options.push(optionName === expression 
-						? `${optionName}` 
-						: `${optionName}: ${expression}`
-					);
-					replacement = replacement.replace(`\${${expression}`, `{${optionName}`);
-				}
-				const quote = trimedSelectedText.includes("'") ? '"' : "'";
-				replacement = `${replacement}, { ${options.join(', ')} }`.replaceAll('`', quote);
-			}
-
-			replacement = `intl.formatMessage(${replacement})`;
+			const replacement = refactorMessage(selectedText);
 
 			editor.edit(editBuilder => {
 				editBuilder.replace(selection, replacement);
@@ -48,6 +19,39 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+export function refactorMessage(selectedText: string): string {
+	const trimedSelectedText = selectedText.slice(1, selectedText.length-1); // exclude quotes
+
+	let replacement = `{ defaultMessage: ${selectedText} }`;
+
+	// Handle template strings
+	const quasis = selectedText.match(/\$\{[^\}]*\}/mg);
+	if (quasis?.length) {
+		let i = 1;
+		const options = [];
+		for (const quasi of quasis) {
+			const expression = quasi.slice(2, quasi.length-1);
+
+			// Build an "ok" option name for the expression
+			let optionName = camelCase(expression.replace('this.', ''));
+			if (optionName.length > 20) {
+				optionName = `option${i}`;
+				i++;
+			}
+
+			options.push(optionName === expression 
+				? `${optionName}` 
+				: `${optionName}: ${expression}`
+			);
+			replacement = replacement.replace(`\${${expression}`, `{${optionName}`);
+		}
+		const quote = trimedSelectedText.includes("'") ? '"' : "'";
+		replacement = `${replacement}, { ${options.join(', ')} }`.replaceAll('`', quote);
+	}
+
+	return `intl.formatMessage(${replacement})`;
 }
 
 export function deactivate() {}
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,60 @@
 import * as assert from 'assert';
+import { refactorMessage } from '../extension';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+suite('Formatjs Shape Test Suite', () => {
+	test('it formats a simple string', () => {
+		const refactor = refactorMessage(`'Formatjs Shape Tests'`);
+		assert.strictEqual(
+			refactor,
+			`intl.formatMessage({ defaultMessage: 'Formatjs Shape Tests' })`
+		);
+	});
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+	test('it formats a string with double quotes', () => {
+		const refactor = refactorMessage(`"Let's test some template strings!"`);
+		assert.strictEqual(
+			refactor,
+			`intl.formatMessage({ defaultMessage: "Let's test some template strings!" })`
+		);
+	});
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('it formats a template string containing an identifier', () => {
+		const refactor = refactorMessage(`\`Hello \${username}\``);
+		assert.strictEqual(
+			refactor,
+			`intl.formatMessage({ defaultMessage: 'Hello {username}' }, { username })`
+		);
+	});
+
+	test('it formats a template string containing a simple quote', () => {
+		const refactor = refactorMessage(`\`Hello \${username}, let's listen to some songs!\``);
+		assert.strictEqual(
+			refactor,
+			`intl.formatMessage({ defaultMessage: "Hello {username}, let's listen to some songs!" }, { username })`
+		);
+	});
+
+	test('it formats a template string containing a call expression', () => {
+		const refactor = refactorMessage(`\`List of songs: \${this.getSongs()}\``);
+		assert.strictEqual(
+			refactor,
+			`intl.formatMessage({ defaultMessage: 'List of songs: {getSongs}' }, { getSongs: this.getSongs() })`
+		);
+	});
+
+	test('it formats a template string containing a long piece of code', () => {
+		const refactor = refactorMessage(`\`My favorite songs: \${this.getSongs().filter((song) => song.isFavirite())}\``);
+		assert.strictEqual(
+			refactor,
+			`intl.formatMessage({ defaultMessage: 'My favorite songs: {option1}' }, { option1: this.getSongs().filter((song) => song.isFavirite()) })`
+		);
+	});
+
+	test('it formats a template string containing several quasis', () => {
+		const refactor = refactorMessage(`\`Here are my favorite songs: \${this.getSongs().filter((song) => song.isFavirite())}. Hey \${username}, these are not my favorite, but do you like them? \${this.getSongs().filter((song) => !song.isFavirite())}\``);
+		assert.strictEqual(
+			refactor,
+			`intl.formatMessage({ defaultMessage: 'Here are my favorite songs: {option1}. Hey {username}, these are not my favorite, but do you like them? {option2}' }, { option1: this.getSongs().filter((song) => song.isFavirite()), username, option2: this.getSongs().filter((song) => !song.isFavirite()) })`
+		);
 	});
 });
